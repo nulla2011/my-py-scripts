e788fdc2483bd2a8331316e83b7e13338baeddbe
nulla2011 <nulla@outlook.com>
2020-12-09 12:50:10 +0800

    new

---
+++ sc-animate.py
@@ -0,0 +1,30 @@
+import requests
+
+url_static = "https://campaign-shinycolors.idolmaster.jp/ssr2020/static/media/"
+
+
+def is404(url):
+    n = requests.head(url, allow_redirects=False)
+    if (n.status_code == 404):
+        return True
+    else:
+        return False
+
+
+for n in range(1, 24):
+    i = 1
+    while True:
+        url = f"{url_static}{str(n).zfill(2)}_{i}memory.mp4"
+        if is404(url):
+            break
+        with open("list.txt", "a+") as f:
+            f.write(url + "\n")
+        i += 1
+    i = 1
+    while True:
+        url = f"{url_static}{str(n).zfill(2)}_{i}staging.mp4"
+        if is404(url):
+            break
+        with open("list.txt", "a+") as f:
+            f.write(url + "\n")
+        i += 1
